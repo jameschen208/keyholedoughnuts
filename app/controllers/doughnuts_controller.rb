970973be6155754aab0d81548b47405ad170648b
James Chen <james.chen208@gmail.com>
2015-11-03 14:38:54 -0500

    added crud abilities for doughnuts

---
+++ app/controllers/doughnuts_controller.rb
@@ -0,0 +1,48 @@
+class DoughnutsController < ApplicationController
+	before_action :find_doughnut, only: [:show, :edit, :update, :destroy]
+	def index
+		@doughnuts = Doughnut.all.order("created_at desc")
+	end
+
+	def new
+		@doughnut = Doughnut.new
+	end
+
+	def create
+		@doughnut = Doughnut.new doughnut_params
+		if @doughnut.save
+			redirect_to @doughnut, notice: "your doughnut was successfully saved"
+		else
+			render 'new'
+		end
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+		if @doughnut.update doughnut_params
+			redirect_to @doughnut, notice: "your doughnut was successfully updated"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@doughnut.destroy
+		redirect_to doughnuts_path
+	end
+	
+	private
+
+	def find_doughnut
+		@doughnut = Doughnut.find(params[:id])
+	end
+	def doughnut_params
+		params.require(:doughnut).permit(:title, :description)
+	end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151102172200) do
+ActiveRecord::Schema.define(version: 20151103190630) do
+
+  create_table "doughnuts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
